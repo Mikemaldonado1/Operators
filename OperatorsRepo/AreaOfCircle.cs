0e69f393b6a59b628caa14e8724da46d5ca22921
Michael Maldonado <MichaelFrankMaldonado@gmail.com>
2022-11-28 08:37:33 -0700

    exercises

---
+++ OperatorsRepo/AreaOfCircle.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace OperatorsRepo
+{
+	public class AreaOfCircle
+	{
+		
+			public static double Area(Double Number)
+		{
+
+			Double Answer = Math.PI * Number * Number;
+			return Answer;
+
+		}
+
+		
+	}
+}
+
---
+++ OperatorsRepo/Program.cs
@@ -0,0 +1,40 @@
+﻿
+
+//Console.WriteLine("Hello, World!");
+
+{
+    int a = 17;
+    int b = 4;
+    int quotient = a / b;
+    int remainder = a % b;
+
+    Console.WriteLine(a + b);
+    Console.WriteLine(a - b);
+    Console.WriteLine(a * b);
+    Console.WriteLine(a / b);
+    Console.WriteLine(a % b);
+    Console.WriteLine();
+    Console.WriteLine();
+
+    Console.WriteLine($"17/4 is {quotient} remainder {remainder}");
+
+    Console.WriteLine();
+    Console.WriteLine();
+
+
+   
+    Console.WriteLine("What is the radius of your circle? ");
+    var radius = double.Parse(Console.ReadLine());
+
+    Console.WriteLine();
+    Console.WriteLine();
+
+
+    
+    Double Answer = OperatorsRepo.AreaOfCircle.Area (radius);
+
+    Console.WriteLine($"The area of a circle with radius of {radius} is {Answer}");
+
+   
+
+}
